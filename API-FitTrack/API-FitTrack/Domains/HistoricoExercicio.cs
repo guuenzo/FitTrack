f50081c1f661b1c6a908da0cff97112f3228683b
kauameloo <kaua.rodrigues2620@gmail.com>
2024-05-27 17:23:18 -0300

    Banco de Dados criado e Domains criada

---
+++ API-FitTrack/API-FitTrack/Domains/HistoricoExercicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_FitTrack.Domains;
+
+public partial class HistoricoExercicio
+{
+    public Guid IdHistoricoExercicio { get; set; }
+
+    public int Repeticoes { get; set; }
+
+    public int Series { get; set; }
+
+    public decimal Carga { get; set; }
+
+    public Guid UsuarioIdUsuario { get; set; }
+
+    public virtual Usuario UsuarioIdUsuarioNavigation { get; set; } = null!;
+}
---
+++ API-FitTrack/API-FitTrack/Domains/HistoricoTreino.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_FitTrack.Domains;
+
+public partial class HistoricoTreino
+{
+    public Guid IdHistoricoTreino { get; set; }
+
+    public DateOnly Data { get; set; }
+
+    public string? Observacao { get; set; }
+
+    public Guid UsuarioIdUsuario { get; set; }
+
+    public Guid TreinoExercicioIdTreinoExercicio { get; set; }
+
+    public virtual TreinoExercicio TreinoExercicioIdTreinoExercicioNavigation { get; set; } = null!;
+
+    public virtual Usuario UsuarioIdUsuarioNavigation { get; set; } = null!;
+}
