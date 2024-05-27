ee0a8bd41198d3d5aaed733bc7fa73dca8b71c11
Gustavo Enzo Santos Aguilar <49816214857@info.local>
2024-05-27 17:25:26 -0300

    .

--- API-FitTrack/API-FitTrack/obj/Debug/net8.0/API-FitTrack.AssemblyInfo.cs
+++ API-FitTrack/API-FitTrack/obj/Debug/net8.0/API-FitTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-FitTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0a7e71b732478c9cace84d7da17914c07fbe06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1bcda3a70bed9110819dd860bfe7ac73adb68da")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-FitTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-FitTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
