f4132434b06f4a0ab0498fb1facb244f83817f5a
Gustavo Enzo Santos Aguilar <49816214857@info.local>
2024-06-05 15:10:00 -0300

    .

---
+++ API-FitTrack/API-FitTrack/Domains/Exercicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_FitTrack.Domains;
+
+public partial class Exercicio
+{
+    public Guid IdExercicio { get; set; }
+
+    public string NomeExercicio { get; set; } = null!;
+
+    public string? Descricao { get; set; }
+
+    public string GrupoMuscular { get; set; } = null!;
+
+    public string? VideoExecucao { get; set; }
+
+    public int Repeticoes { get; set; }
+
+    public decimal Carga { get; set; }
+
+    public int Series { get; set; }
+
+    public string? FotoExercicio { get; set; }
+
+    public virtual ICollection<TreinoExercicio> TreinoExercicios { get; set; } = new List<TreinoExercicio>();
+}
