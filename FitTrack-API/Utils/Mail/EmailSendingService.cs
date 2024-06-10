4c9b5906c15953a273e0a0df1353210f880c70d9
kauameloo <kaua.rodrigues2620@gmail.com>
2024-06-10 15:14:42 -0300

    html email sending feito

--- FitTrack-API/Utils/Mail/EmailSendingService.cs
+++ FitTrack-API/Utils/Mail/EmailSendingService.cs
@@ -20,7 +20,7 @@ namespace API_FitTrack.Utils.Mail
                     MailRequest request = new MailRequest()
                     {
                         ToEmail = email,
-                        Subject = "Bem vindo ao VitalHub",
+                        Subject = "Bem vindo ao FitTrack",
                         Body = GetHtmlContent(userName)
                     };
 
@@ -64,16 +64,16 @@ namespace API_FitTrack.Utils.Mail
         {
             // Constrói o conteúdo HTML do e-mail, incluindo o nome do usuário
             string Response = @"
-<div style=""width:100%; background-color:rgba(96, 191, 197, 1); padding: 20px;"">
+<div style=""width:100%; background-color:rgba(43, 60, 100, 1); padding: 20px;"">
     <div style=""max-width: 600px; margin: 0 auto; background-color:#FFFFFF; border-radius: 10px; padding: 20px;"">
-        <img src=""https://blobvitalhub.blob.core.windows.net/containervitalhub/logotipo.png"" alt="" Logotipo da Aplicação"" style="" display: block; margin: 0 auto; max-width: 200px;"" />
-        <h1 style=""color: #333333; text-align: center;"">Bem-vindo ao VitalHub!</h1>
+        <img src=""https://blobvitalhubg16enzo.blob.core.windows.net/containerfittrack/logo.png"" alt="" Logotipo da Aplicação"" style="" display: block; margin: 0 auto; max-width: 200px;"" />
+        <h1 style=""color: #333333; text-align: center;"">Bem-vindo ao FitTrack!</h1>
         <p style=""color: #666666; text-align: center;"">Olá <strong>" + userName + @"</strong>,</p>
-        <p style=""color: #666666;text-align: center"">Estamos muito felizes por você ter se inscrito na plataforma VitalHub.</p>
+        <p style=""color: #666666;text-align: center"">Estamos muito felizes por você ter se inscrito na plataforma FitTrack.</p>
         <p style=""color: #666666;text-align: center"">Explore todas as funcionalidades que oferecemos e encontre os melhores médicos.</p>
         <p style=""color: #666666;text-align: center"">Se tiver alguma dúvida ou precisar de assistência, nossa equipe de suporte está sempre pronta para ajudar.</p>
         <p style=""color: #666666;text-align: center"">Aproveite sua experiência conosco!</p>
-        <p style=""color: #666666;text-align: center"">Atenciosamente,<br>Equipe VitalHub</p>
+        <p style=""color: #666666;text-align: center"">Atenciosamente,<br>Equipe FitTrack</p>
     </div>
 </div>";
 
@@ -84,9 +84,9 @@ namespace API_FitTrack.Utils.Mail
         private string GetHtmlContentRecovery(int codigo)
         {
             string Response = @"
-<div style=""width:100%; background-color:rgba(96, 191, 197, 1); padding: 20px;"">
+<div style=""width:100%; background-color:rgba(43, 60, 100, 1); padding: 20px;"">
     <div style=""max-width: 600px; margin: 0 auto; background-color:#FFFFFF; border-radius: 10px; padding: 20px;"">
-        <img src=""https://blobvitalhub.blob.core.windows.net/containervitalhub/logotipo.png"" alt="" Logotipo da Aplicação"" style="" display: block; margin: 0 auto; max-width: 200px;"" />
+        <img src=""https://blobvitalhubg16enzo.blob.core.windows.net/containerfittrack/logo.png"" alt="" Logotipo da Aplicação"" style="" display: block; margin: 0 auto; max-width: 200px;"" />
         <h1 style=""color: #333333;text-align: center;"">Recuperação de senha</h1>
         <p style=""color: #666666;font-size: 24px; text-align: center;"">Código de confirmação <strong>" + codigo + @"</strong></p>
     </div>
--- FitTrack-API/obj/Debug/net8.0/FitTrack-API.AssemblyInfo.cs
+++ FitTrack-API/obj/Debug/net8.0/FitTrack-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitTrack-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c30de7487b87298340ce7cebd1e4e708e7b1b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30be2da8ca92ceb273622490b2931c2a475395a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitTrack-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitTrack-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
